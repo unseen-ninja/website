e66eb81a590913a60a51c9b9531a7c66b5bb5657
lemon <35840154+unseen-ninja@users.noreply.github.com>
2024-05-11 23:30:46 +0000

    chore: init MVP (#1)
    Co-authored-by: lemon <35840154+andreasgrafen@users.noreply.github.com>

---
+++ src/components/MusicPlayer.vue
@@ -0,0 +1,120 @@
+<script setup lang="ts">
+
+  import { ref } from 'vue'
+
+
+  let audio = new (AudioContext)(),
+      src: string = '/assets/music/electrostatic.mp3',
+      trackName: string = 'lemon — Electrostatic',
+      pausedText: string = 'Click to play Music',
+      trackInfo = ref(pausedText),
+      audioData: any, playerNode: any
+
+  const createAudio = (audioBuffer: any) => {
+    if (!audioData) audioData = audioBuffer
+  }
+
+  const decode = (buffer: any) => audio.decodeAudioData(buffer, createAudio)
+
+  fetch(src, {mode: "cors"})
+    .then((response) => response.arrayBuffer())
+    .then(decode)
+
+  const volume = audio.createGain()
+        volume.gain.value = 0.25
+        volume.connect(audio.destination)
+
+  function playLoop(audioBuffer: any) {
+    playerNode = audio.createBufferSource()
+    playerNode.buffer = audioBuffer
+    playerNode.connect(volume)
+    playerNode.loop = true
+    playerNode.start()
+  }
+
+  function toggleMusic() {
+
+    const animationPlay  = document.getElementById('animate-play'),
+          animationPause = document.getElementById('animate-pause')
+
+    if (playerNode) {
+      // @ts-ignore
+      animationPause.beginElement()
+      trackInfo.value = pausedText
+      playerNode.stop()
+      playerNode = null
+    }
+
+    else {
+      // @ts-ignore
+      animationPlay.beginElement()
+      trackInfo.value = trackName
+      playLoop(audioData)
+    }
+
+  }
+
+</script>
+
+
+
+
+
+<template>
+  <div id="player">
+
+    <small id="track-info">{{ trackInfo }}</small>
+
+    <button @click="toggleMusic();" id="play-button" aria-live="assertive" tabindex="1" aria-label="Play/Pause">
+      <svg width="100%" viewBox="0 0 36 36" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+          <defs>
+            <path id="ytp-12" d="M11,10 L18,13.74 18,22.28 11,26 M18,13.74 L26,18 26,18 18,22.28">
+              <animate id="animate-play" begin="indefinite" attributeName="d" dur="200ms" fill="freeze" keySplines=".4 0 1 1" from="M11,10 L18,13.74 18,22.28 11,26 M18,13.74 L26,18 26,18 18,22.28" to="M11,10 L17,10 17,26 11,26 M20,10 L26,10 26,26 20,26"></animate>
+              <animate id="animate-pause" begin="indefinite" attributeName="d" dur="200ms" fill="freeze" keySplines=".4 0 1 1" from="M11,10 L17,10 17,26 11,26 M20,10 L26,10 26,26 20,26" to="M11,10 L18,13.74 18,22.28 11,26 M18,13.74 L26,18 26,18 18,22.28"></animate>
+            </path>
+          </defs>
+          <use xlink:href="#ytp-12" class="ytp-svg-fill"></use>
+      </svg>
+    </button>
+
+  </div>
+</template>
+
+
+
+
+
+<style scoped lang="scss">
+
+  #player {
+
+    display: flex;
+    align-items: center;
+
+  }
+
+
+  #player #play-button {
+
+    transform: translate(-.5px, 4px);
+
+    margin: 0; padding: 0;
+
+    border: none;
+    background-color: transparent;
+
+
+    svg * { transition: fill .1s; }
+
+  }
+
+
+  a:has(#track-info) { color: var(--text-muted); }
+  #player #track-info { padding-inline-start: var(--spacing-sm); }
+
+
+  #play-button svg { height: 4.8rem; }
+  #play-button svg > * { fill: var(--text-highlight); }
+  #play-button:hover svg > * { fill: var(--peach); }
+
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,110 @@
+<script setup lang="ts">
+
+  import Wave from '../components/WavingHand.vue'
+
+</script>
+
+
+
+
+
+<template>
+
+  <article id="about">
+
+    <p class="pre-heading">About Me</p>
+    <h1>Hi! My name's <span class="text-lemonade">lemon</span>. <Wave :size=24 /></h1>
+
+    <p>and I'm »[…] just a regular everyday normal guy« working on design and code thingies like <a href="https://catppuccin.com">Catppuccin</a> – well, sometimes. <span class="text-lemonade">#opensource</span> advocate and according to League of Graphs: <strong><span class="text-lemonade">a top 1% support player.</span></strong></p>
+    <p>Always quite keen on new challenges I spent the past 15-ish years digging into design principles and web technologies – always trying to stay up to date with the latest and greatest design trends and tech stuff the ever-changing digital landscape – we all grew so accustomed to – has to offer.</p>
+    <p>Nowadays I don't get to work too much on web stuff as my main job is in communications, social media and press work. But whenever I get the chance I'll dig myself into some nice HTML, CSS and JavaScript.</p>
+    <p>Besides web dev I also dabble into Linux <small><em>ackchyually it's GNU/Linux??! I use arch btw.</em></small>, home and process automation, server side stuff, and cyber security.</p>
+    <p>Outside the tech space or well… at least partially I enjoy video games, hiking <small><em>really depends on my mood tho</em></small> or just reading a good book.</p>
+
+  </article>
+
+</template>
+
+
+
+
+
+<style scoped lang="scss">
+
+  .image {
+
+    position: relative;
+    float: right;
+    isolation: isolate;
+
+    display: grid;
+    place-items: center;
+
+    width: 300px; aspect-ratio: 1/1;
+    overflow: clip;
+
+    border-radius: var(--spacing-sm);
+    background-color: var(--blackberry);
+    background-image: url('/assets/images/noise.png');
+    background-position: center;
+    background-blend-mode: overlay;
+
+    font-size: 200%; font-weight: 900;
+    color: var(--lychee-tint);
+
+
+    img {
+
+      position: absolute;
+      z-index: -3;
+
+      opacity: .5;
+
+      width: 100%; height: 100%;
+      object-fit: cover;
+
+      background-color: var(--blackberry);
+      background-image: url('/assets/images/noise.png');
+      background-position: center;
+      background-blend-mode: overlay;
+
+    }
+
+
+    &::before,
+    &::after {
+
+      content: '';
+
+      position: absolute;
+      inset: 0;
+
+    }
+
+
+    &::before {
+
+      z-index: -1;
+
+      background-color: var(--lemon);
+      background-image: var(--lemon-gradient);
+      mix-blend-mode: multiply;
+
+    }
+
+
+    &::after {
+
+      z-index: -2;
+
+      background-color: var(--grape);
+      background-image: var(--grape-gradient);
+      mix-blend-mode: screen;
+
+    }
+
+
+
+  }
+
+</style>
