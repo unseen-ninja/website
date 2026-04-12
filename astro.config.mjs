0d35d72912169b19841a5731789b38854b48dbaf
lemon <git@unseen.ninja>
2026-04-12 23:39:17 +0200

    chore: ready for lift-off!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 
+import icon from 'astro-icon';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	site: 'https://unseen.ninja',
+	integrations: [icon(), mdx()],
 });
---
+++ src/data/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_AUTHOR = 'unseen;ninja';
+export const SITE_TITLE = 'Design & Code';
+export const SITE_DESCRIPTION = '»[…] just a regular everyday normal guy« working on design and code thingies.';
